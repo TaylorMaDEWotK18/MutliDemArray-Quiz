9cf3d635d3d1392413b0590ba3d19f69ecfe9ba4
TaylorMaDEWotK18 <84547566+TaylorMaDEWotK18@users.noreply.github.com>
2022-03-03 15:19:20 -0700

    Add files via upload
    Finally uploaded files to github

---
+++ js/alert.js
@@ -0,0 +1,70 @@
+/* 
+  1. Store correct answers
+   - When quiz begins, no answers are correct
+*/
+let correct = 0;
+
+//2. Store the rank of a player
+const gold = 5;
+const silver = (3 || 4);
+const bronze = (1 || 2);
+const noCrown = 0;
+
+// 3. Select the <main> HTML element
+
+
+
+/*
+  4. Ask at least 5 questions
+   - Store each answer in a variable
+   - Keep track of the number of correct answers
+*/
+const q1 = prompt("What is the demon's name that kills the fire Hashira?");
+if ( q1.toUpperCase() === 'AKAZA' ) {
+  correct += 1;
+} 
+console.log(correct);
+let q2 = prompt("What is Tanjiro's first trainer's name? (Proper spelling)");
+if ( q2 === 'Urokodaki' ) {
+  correct += 1;
+}
+console.log(correct);
+let q3 = prompt("How many arms does Sasumaru (ball throwing demon) truly have?")
+if ( q3 === `6`) {
+  correct += 1;
+}
+console.log(correct);
+let q4 = prompt("What color does Tanjiro's sword turn?");
+if ( q4 === 'black' ) {
+  correct += 1;
+}
+console.log(correct);
+let q5 = prompt("This one's a toughie! What is Tanjiro's flame form really called? Good luck!");
+if ( q5.toUpperCase() === 'HINOKAMI KAGURA' ) {
+  correct += 1;
+} 
+console.log(correct);
+
+const totalScore = `<h1> You got ${correct} out of 5 </h1>
+                   <h2> Crown earned:</h2>`;
+
+/*
+  5. Rank player based on number of correct answers
+   - 5 correct = Gold
+   - 3-4 correct = Silver
+   - 1-2 correct = Bronze
+   - 0 correct = No crown
+*/
+if ( correct === 5 ) {
+  alert("AND HE TAKES THE GOLD FOLKS!!! TOP SCORE!");
+} else  if ( correct === 4 || correct === 3 ) {
+  alert("AND HE TAKES THE SILVER FOLKS!!!");
+} else if ( correct === 1 || correct === 2 ) {
+  alert("Uwww, so sorry.. You're goin home with the Bronze today.");
+} else {
+  alert("You SUCK! You got a big fat ZERO! Re-watch and try again!");
+}
+
+
+// 6. Output results to the <main> element
+document.querySelector('main').innerHTML = totalScore;
